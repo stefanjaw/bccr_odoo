1fa1c626aa0e38e702aa8b4c938226750f57224c
stefanjaw <40605674+stefanjaw@users.noreply.github.com>
2024-01-23 21:19:40 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.2.17',
 
     # any module necessary for this one to work correctly
     'depends': ['base','currency_rate_live'],
